aa3eb7e8b8b67f250beac647bc52ab69e458d5d9
Class account - UAL <ualclsd0173@asaxlogin2.asc.edu>
2024-10-13 16:46:35 -0500

    editing scripts

--- myscript.sh
+++ myscript.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 module load intel
 ./hw3 5000 5000 1 /scratch/ualclsd0173 1
-./hw3 5000 5000 1 /scratch/ualclsd0173 1
-./hw3 5000 5000 1 /scratch/ualclsd0173 1
-./hw3 5000 5000 2 /scratch/ualclsd0173 1
-./hw3 5000 5000 2 /scratch/ualclsd0173 1
 ./hw3 5000 5000 2 /scratch/ualclsd0173 1
 ./hw3 5000 5000 4 /scratch/ualclsd0173 1
-./hw3 5000 5000 4 /scratch/ualclsd0173 1
-./hw3 5000 5000 4 /scratch/ualclsd0173 1
-./hw3 5000 5000 8 /scratch/ualclsd0173 1
-./hw3 5000 5000 8 /scratch/ualclsd0173 1
 ./hw3 5000 5000 8 /scratch/ualclsd0173 1
 ./hw3 5000 5000 10 /scratch/ualclsd0173 1
-./hw3 5000 5000 10 /scratch/ualclsd0173 1
-./hw3 5000 5000 10 /scratch/ualclsd0173 1
 ./hw3 5000 5000 16 /scratch/ualclsd0173 1
-./hw3 5000 5000 16 /scratch/ualclsd0173 1
-./hw3 5000 5000 16 /scratch/ualclsd0173 1
-./hw3 5000 5000 20 /scratch/ualclsd0173 1
-./hw3 5000 5000 20 /scratch/ualclsd0173 1
 ./hw3 5000 5000 20 /scratch/ualclsd0173 1
